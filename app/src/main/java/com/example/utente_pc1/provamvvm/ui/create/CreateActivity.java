bd1aace70aab7c8e874e26d25a39d24dcabc2933
noh <aeonss@live.com>
2018-05-08 14:54:44 +0200

    Create Ui changes

--- app/src/main/java/com/example/utente_pc1/provamvvm/ui/create/CreateActivity.java
+++ app/src/main/java/com/example/utente_pc1/provamvvm/ui/create/CreateActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.utente_pc1.provamvvm.R;
 import com.example.utente_pc1.provamvvm.model.data.ListItemSubj;
@@ -42,35 +43,46 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
         calendarView = (CalendarView)findViewById(R.id.cal_date);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                curdate = dayOfMonth +"/" + month + "/" + year;
+                curdate = dayOfMonth + "/" + (month + 1) + "/" + year;
             }
         });
+
         spinner = (Spinner)findViewById(R.id.spin_subj);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.subjs_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
         hours = (EditText)findViewById(R.id.txtcr_hours);
+
         addbtn = (Button) findViewById(R.id.btncr);
 
         db = DbGuardian.subjectdb;
         dao = db.ListItemSubjDao();
         repository = new SubjectRepository(dao);
         wFactory = new CustomViewModelFactory(repository);
+
         addbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SimpleDateFormat conv = new SimpleDateFormat("dd/MM/yy");
+                String defdate = conv.format(calendarView.getDate());
                 ListItemSubj tmp = new ListItemSubj();
-                tmp.setHours(Integer.valueOf(hours.getText().toString()));
-                tmp.setDate(curdate);
-                tmp.setName(spinner.getSelectedItem().toString());
-                wFactory.create(CreateItemViewModel.class).insertSubj(tmp);
-                startListActivity();
+                try {
+                    tmp.setHours(Integer.valueOf(hours.getText().toString()));
+                    tmp.setDate((curdate != null) ? curdate : defdate);
+                    tmp.setName(spinner.getSelectedItem().toString());
+                    wFactory.create(CreateItemViewModel.class).insertSubj(tmp);
+                    startListActivity();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "You have to select all info!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
